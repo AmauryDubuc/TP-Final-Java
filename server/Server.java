a35f6d437b84fc743d679a4f0e46cf8ec84b8734
goronc <108907603+goronc@users.noreply.github.com>
2023-09-11 17:14:12 +1100

    wip 11/09

---
+++ server/Server.java
@@ -0,0 +1,34 @@
+package univ.nc.fx.network.tcp.tchat.server;
+
+import javafx.application.Platform;
+import univ.nc.fx.network.tcp.tchat.ITchat;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Processus serveur qui ecoute les connexion entrantes,
+ * les messages entrant et les rediffuse au clients connectes
+ *
+ * @author mathieu.fabre
+ */
+public class Server extends Thread implements ITchat {
+
+	// TODO
+	// TODO
+	// TODO
+	
+    /**
+     * Envoi un message de log a l'IHM
+     */
+    public void sendLogToUI(String message) {
+        Platform.runLater(() -> serverUI.log(message));
+    }
+
+}
